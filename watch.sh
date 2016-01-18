d8e779246d59c45f73f08d57e33a0eff7462ffab
Michael Egorov <michwill@gmail.com>
2016-01-18 14:49:47 +0000

    What is that watch script anyway?

--- watch.sh
+++ watch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script compiles the document as soon as all the .tex files in the project are updated.
+# In debian-based distros, it needs inotify-tools and (optionally) libnotify-bin
+
 inotifywait -mr -e create ./ | while read dir event file; do
     if [[ $file =~ .*\.tex ]]
     then
